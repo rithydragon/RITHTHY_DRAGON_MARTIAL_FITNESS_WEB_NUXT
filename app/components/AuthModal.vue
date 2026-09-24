5e47bdf3c7b86c79b7dc89f64f752edf3e2fb394
rithydragon <riththy.learn@gmail.com>
2026-09-25 01:39:21 +0700

    fd

--- app/components/AuthModal.vue
+++ app/components/AuthModal.vue
@@ -309,11 +309,11 @@ function injectTelegramWidget() {
   }
   const script = document.createElement('script')
   script.async = true
-  script.src = 'https://telegram.org/js/telegram-widget.js?22'
+  script.src = 'https://telegram.org/js/telegram-widget.js?23'
   script.setAttribute('data-telegram-login', twBot.value)
   script.setAttribute('data-size', 'large')
   script.setAttribute('data-radius', '12')
-  script.setAttribute('data-javascript-callback', 'onTelegramAuth')
+  script.setAttribute('data-onauth', 'onTelegramAuth(user)')
   el.appendChild(script)
 }
 
--- app/pages/telegram-widget.vue
+++ app/pages/telegram-widget.vue
@@ -72,11 +72,11 @@ onMounted(async () => {
 
     const script = document.createElement('script')
     script.async = true
-    script.src = 'https://telegram.org/js/telegram-widget.js?22'
+    script.src = 'https://telegram.org/js/telegram-widget.js?23'
     script.setAttribute('data-telegram-login', bot)
     script.setAttribute('data-size', 'large')
     script.setAttribute('data-radius', '12')
-    script.setAttribute('data-javascript-callback', 'onTelegramAuth')
+    script.setAttribute('data-onauth', 'onTelegramAuth(user)')
     widgetEl.value?.appendChild(script)
   } catch {
     error.value = t('auth.telegramUnavailable')
